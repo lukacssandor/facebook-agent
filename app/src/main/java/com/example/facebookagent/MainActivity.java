891ef75a6b08b89dcc73297f8fd77323124d3d67
Lukács Sándor <lukacssandor98@gmail.com>
2019-05-11 20:38:45 +0200

    upload project

---
+++ app/src/main/java/com/example/facebookagent/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.facebookagent;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String LOGIT_RESULT_ID = "com.example.facebookagent.LOGIN_RESULT_ID";
+    public static final String LOGIN_RESULT_NAME = "com.example.facebookagent.LOGIN_RESULT_NAME";
+    public static final String LOGIN_RESULT_EMAIL = "com.example.facebookagent.LOGIN_RESULT_EMAIL";
+    public static final String LOGIN_RESULT_BIRTH = "com.example.facebookagent.LOGIN_RESULT_BIRTH";
+    public static final String LOGIN_RESULT_HOMETOWN = "com.example.facebookagent.LOGIN_RESULT_HOMETOWN";
+    public static final String LOGIN_RESULT_FRIENDS = "com.example.facebookagent.LOGIN_RESULT_FRIENDS";
+    public static final String TAG = "fb_log";
+
+    CallbackManager callbackManager;
+    ProgressDialog mDialog;
+    Intent intent;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.facebookagent",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        final LoginButton loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("public_profile", "email", "user_birthday", "user_friends, user_hometown"));
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                mDialog = new ProgressDialog(MainActivity.this);
+                mDialog.setMessage("Waiting for data...");
+                mDialog.show();
+
+                AccessToken accesstoken = loginResult.getAccessToken();
+                Log.d(TAG, accesstoken.toString());
+                GraphRequest request = GraphRequest.newMeRequest(accesstoken, new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        mDialog.dismiss();
+
+                        try {
+                            Log.d(TAG, object.toString());
+                            sendResult(object);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id, name, email, birthday, friends, hometown");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.e(TAG, "onError", error);
+            }
+        });
+    }
+
+    private void sendResult(JSONObject object) throws JSONException {
+        intent = new Intent(this, PageActivity.class);
+        intent.putExtra(LOGIT_RESULT_ID, object.getString("id"));
+        intent.putExtra(LOGIN_RESULT_NAME, object.getString("name"));
+        intent.putExtra(LOGIN_RESULT_EMAIL, object.getString("email"));
+        intent.putExtra(LOGIN_RESULT_BIRTH, object.getString("birthday"));
+        intent.putExtra(LOGIN_RESULT_HOMETOWN, object.getJSONObject("hometown").getString("name"));
+        intent.putExtra(LOGIN_RESULT_FRIENDS, object.getJSONObject("friends").getJSONObject("summary").getString("total_count"));
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/facebookagent/PageActivity.java
@@ -0,0 +1,142 @@
+package com.example.facebookagent;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.login.LoginManager;
+import com.squareup.picasso.Picasso;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import jp.wasabeef.picasso.transformations.CropCircleTransformation;
+
+public class PageActivity extends AppCompatActivity {
+
+    TextView name, birthday, email, hometown, friendsCount;
+    ImageView avatar;
+    AccessTokenTracker accessTokenTracker;
+    ProgressDialog mDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page);
+
+        Intent intent = getIntent();
+
+        name = findViewById(R.id.id_name);
+        birthday = findViewById(R.id.id_birth);
+        email = findViewById(R.id.id_email);
+        hometown = findViewById(R.id.id_hometown);
+        friendsCount = findViewById(R.id.id_friend_count);
+
+        avatar = findViewById(R.id.id_avatar);
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken,
+                                                       AccessToken currentAccessToken) {
+                if (currentAccessToken == null) {
+                    Log.d(MainActivity.TAG, oldAccessToken.toString());
+                    Log.d(MainActivity.TAG, "onLogout catched");
+                    disconnectFromFacebook(oldAccessToken);
+                }
+            }
+        };
+
+        try {
+            URL avatar_picture_url = new URL("https://graph.facebook.com/" +
+                                            intent.getStringExtra(MainActivity.LOGIT_RESULT_ID) +
+                                            "/picture?width=250&height=250");
+
+            Picasso.get()
+                    .load(avatar_picture_url.toString())
+                    .transform(new CropCircleTransformation())
+                    .resize(250, 250)
+                    .into(avatar);
+
+            String nameStr = intent.getStringExtra(MainActivity.LOGIN_RESULT_NAME);
+            SpannableStringBuilder nameSSB = new SpannableStringBuilder(nameStr);
+            nameSSB.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), 0, nameStr.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            name.setText(nameSSB);
+
+            String emailStamp = "Email: ";
+            String emailStr = emailStamp + intent.getStringExtra(MainActivity.LOGIN_RESULT_EMAIL);
+            SpannableStringBuilder emailSSB = new SpannableStringBuilder(emailStr);
+            emailSSB.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), 0, emailStamp.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            email.setText(emailSSB);
+
+            String birthdayStamp = "Birthday: ";
+            String birthdayStr = birthdayStamp + intent.getStringExtra(MainActivity.LOGIN_RESULT_BIRTH);
+            SpannableStringBuilder birthdaySSB = new SpannableStringBuilder(birthdayStr);
+            birthdaySSB.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), 0, birthdayStamp.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            birthday.setText(birthdaySSB);
+
+            String hometownStamp = "Hometown: ";
+            String hometownStr = hometownStamp + intent.getStringExtra(MainActivity.LOGIN_RESULT_HOMETOWN);
+            SpannableStringBuilder hometownSSB = new SpannableStringBuilder(hometownStr);
+            hometownSSB.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), 0, hometownStamp.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            hometown.setText(hometownSSB);
+
+            String friendsCountStamp = "Friends: ";
+            String friendsCountStr = friendsCountStamp + intent.getStringExtra(MainActivity.LOGIN_RESULT_HOMETOWN);
+            SpannableStringBuilder friendsCountSSB = new SpannableStringBuilder(friendsCountStr);
+            friendsCountSSB.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), 0, friendsCountStamp.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            friendsCount.setText(friendsCountSSB);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        accessTokenTracker.startTracking();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+            Log.d(MainActivity.TAG, "back button pressed");
+            disconnectFromFacebook(AccessToken.getCurrentAccessToken());
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void disconnectFromFacebook(AccessToken accessToken) {
+
+        if (accessToken == null) {
+            Log.d(MainActivity.TAG, "no access token");
+            return;
+        }
+
+        mDialog = new ProgressDialog(this);
+        mDialog.setMessage("Logging out...");
+        mDialog.show();
+        findViewById(R.id.logout_button).setVisibility(View.GONE);
+
+        new GraphRequest(accessToken, "/me/permissions/", null, HttpMethod.DELETE, new GraphRequest
+                .Callback() {
+            @Override
+            public void onCompleted(GraphResponse graphResponse) {
+                Log.d(MainActivity.TAG, "logged out");
+                LoginManager.getInstance().logOut();
+                accessTokenTracker.stopTracking();
+                mDialog.dismiss();
+                finish();
+            }
+        }).executeAsync();
+    }
+}
